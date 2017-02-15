457af2bd6cb41af690b1886b2f039c927f41a766
Will Dewing <dewing.will@gmail.com>
2017-02-14 18:06:05 -0600

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public LayerMask selectableLayer;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Click");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Here is where we would need to check for actions on the object we hit
+                Debug.Log("Move");
+                agent.SetDestination(hit.point);
+            }
+            
+        }
+    }
+}
